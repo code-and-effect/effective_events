15ceb95cd994730bec86e7f262fcb406d9d68d8a
Dana Janssen <dana@codeandeffect.com>
2022-10-29 15:08:17 -0600

    First draft of design for events

--- app/helpers/effective_events_helper.rb
+++ app/helpers/effective_events_helper.rb
@@ -1,4 +1,11 @@
 module EffectiveEventsHelper
+  def effective_events_event_schedule(event)
+    if event.start_at.beginning_of_day == event.end_at.beginning_of_day
+      "#{event.start_at.strftime("%b %d, %Y %I:%M%P")} - #{event.end_at.strftime("%I:%M%P")}"
+    else
+      "#{event.start_at.strftime("%b %d, %Y")} - #{event.end_at.strftime("%b %d, %Y")}"
+    end
+  end
 
   def effective_events_event_tickets_collection(event, namespace = nil)
     raise('expected an Effective::Event') unless event.kind_of?(Effective::Event)
@@ -37,5 +44,4 @@ module EffectiveEventsHelper
       [label, product.to_param, disabled].compact
     end
   end
-
 end
