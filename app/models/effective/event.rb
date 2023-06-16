59720a8fe13fd1cdf32750e3c84c2b3450ea3d42
Leonardo Momente <leom806@gmail.com>
2023-06-16 17:39:39 -0300

    Remove if

--- app/models/effective/event.rb
+++ app/models/effective/event.rb
@@ -17,8 +17,7 @@ module Effective
                         tsearch: {
                           highlight: true,
                         }
-                      },
-                      if: -> (event) { event.published? }
+                      }
 
     end
 
