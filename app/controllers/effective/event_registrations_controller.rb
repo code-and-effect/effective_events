e0d34a9eb7c727553757666c2d9e6684b4ff2615
Matt Riemer <matthew@codeandeffect.com>
2023-12-11 14:17:03 -0700

    validate registrations before checkout

--- app/controllers/effective/event_registrations_controller.rb
+++ app/controllers/effective/event_registrations_controller.rb
@@ -1,11 +1,12 @@
 module Effective
   class EventRegistrationsController < ApplicationController
-
     if defined?(Devise)
       before_action :authenticate_user!, unless: -> { action_name == 'new' || (action_name == 'show' && params[:id] == 'start') }
     end
 
     include Effective::WizardController
+
+    before_action :redirect_unless_registerable, only: [:new, :show]
 
     resource_scope -> {
       event = Effective::Event.find(params[:event_id])
@@ -24,6 +25,16 @@ module Effective
       end
 
       resource.ready!
+    end
+
+    # If the event is no longer registerable, do not let them continue
+    def redirect_unless_registerable
+      return if resource.blank?
+      return if resource.event.blank?
+      return if resource.event.registerable?
+
+      flash[:danger] = "Your selected event is no longer available for registration. This event registration is no longer available."
+      return redirect_to('/dashboard')
     end
 
   end
