7c2288b333c840a17347eea5aa92ae7d494296c7
Matt Riemer <matthew@codeandeffect.com>
2024-12-18 14:08:58 -0700

    Fix test

--- test/models/event_confirmations_test.rb
+++ test/models/event_confirmations_test.rb
@@ -4,6 +4,7 @@ class EventConfirmationsTest < ActiveSupport::TestCase
   test 'event registration confirmation' do
     event_registration = build_event_registration()
     event_registration.event.update!(rich_text_confirmation_email: "<p>Test Content</p>")
+    event_registration.ready!
 
     assert_email(count: 1) do
       event_registration.send_event_registration_confirmation!
