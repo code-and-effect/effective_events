df4546903e47063aa587053df23edf26d18e5b80
Matt Riemer <matthew@codeandeffect.com>
2026-03-04 15:07:45 -0700

    Upgrade test suite

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,13 +1,14 @@
 require_relative "boot"
 
 require "rails/all"
+require 'sprockets/rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'devise'
 require 'haml'
+require 'devise'
 require 'effective_addresses'
 require 'effective_orders'
 require 'effective_email_templates'
@@ -18,7 +19,13 @@ require "effective_test_bot"
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults Rails::VERSION::STRING.to_f
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 8.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -28,6 +35,8 @@ module Dummy
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Required for testing
+    config.active_record.use_yaml_unsafe_load = true
     config.active_job.queue_adapter = :inline
   end
 end
